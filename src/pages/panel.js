4822042760854d28a033809f93a74023bdcec4e6
tayyipcanbay <202013709006@ogr.balikesir.edu.tr>
2023-01-21 16:57:52 +0300

    pusht :D.d.d.

--- src/pages/panel.js
+++ src/pages/panel.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import "../styles/panel.css";
 
 const Panel = () => {
+    const [mail, setMail] = useState("");
+    const [token, setToken] = useState("");
+    const [userId, setUserId] = useState("");
+    const [isLogin, setIsLogin] = useState(false);
+
+    const deleteLocalStorage = () => {
+        localStorage.removeItem("mail");
+        localStorage.removeItem("token");
+        localStorage.removeItem("userId");
+    }
+
+    useEffect(() => {
+        setMail(localStorage.getItem("mail"));
+        setToken(localStorage.getItem("token"));
+        setUserId(localStorage.getItem("userId"));
+        if (mail !== null && token !== null && userId !== null) {
+            setIsLogin(true);
+        } else {
+            setIsLogin(false);
+            deleteLocalStorage();
+            window.location.href = "/";
+        }
+    }, [mail, token, userId]);
+
     return (
-        <div>
-            <h1>Panel</h1>
+        <div className="panel-body">
+            Panel
         </div>
     )
 }
